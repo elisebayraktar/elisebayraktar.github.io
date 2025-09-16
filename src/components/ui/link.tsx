387f23f86c629ce037af43c2cb4f4955b01b12f3
Antoine Bouteiller <anto.bouteiller@gmail.com>
2025-09-16 14:52:53 +0200

    feat: add google verification

--- src/components/ui/link.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from '@/lib/utils'
-
-export default function Link({ className, children, ...props }: React.ComponentProps<'a'>) {
-  return (
-    <a className={cn(className)} {...props}>
-      {children}
-    </a>
-  )
-}
